6196b273b68d3216406c59d631c3f6cb6672297f
johncoryk <coryk0789@gmail.com>
2020-08-08 22:24:54 -0400

    set up buttons to add and see all todos, set up add todo view, buttons working

--- routes/todos-router.js
+++ routes/todos-router.js
@@ -4,6 +4,10 @@ const todosController = require('../controllers/todos-controller');
 todosRouter.get('/', todosController.index);
 todosRouter.post('/', todosController.create);
 
+todosRouter.get('/add', (req, res) => {
+  res.render('todos/add');
+});
+
 todosRouter.get('/:id', todosController.show);
 todosRouter.put('/:id', todosController.update);
 todosRouter.delete('/:id', todosController.delete);
